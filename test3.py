8d9655e2f1c279f60016b8441a57a1881b5fd163
WilliamK7827 <74074978+WilliamK7827@users.noreply.github.com>
2023-05-11 13:01:44 -0700

    adsa

--- test3.py
+++ test3.py
@@ -1,14 +1,3 @@
 
 
-list = ["a", "b", 1, 2.0]
-list.append(True)
-list.insert(4, False)
-del(list[5])
-
-print(list, len(list), list[4], sep="\n")
-
-list2 = ["fehh", "non", "jashdk"]
-
-list2.extend(list)
-
-print(list2 + ['a'])
+# nothing here
