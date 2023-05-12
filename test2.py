7c26ad2a53c5a7d959a9e9b21af31b8346781cc7
Sketchooo <117197566+enluia@users.noreply.github.com>
2023-05-11 23:59:44 -0700

    random Leetcode problems

--- test2.py
+++ test2.py
@@ -1,10 +1,29 @@
-inputFile = open("studentData.csv", "r")
+class Solution:
+    def longestCommonPrefix(strs):
 
-inputArray = []
+        output = ""
+        shortestLength = len(strs[0])
+        shortestString = ""
 
-for line in inputFile:
-    inputArray = line
+        print(shortestLength)
 
-for line in inputArray:
-    print(inputArray[line], end = "")
+        if len(strs) == 1:
+            return strs[0]
 
+        for i in strs:
+            if len(i) <= shortestLength:
+                shortestLength = len(i)
+                shortestString = i
+
+        print(shortestLength)
+        print(shortestString)
+
+        for i in range(len(shortestString)):
+            for j in strs:
+                if j[i] != shortestString[i]:
+                    return output
+            output += shortestString[i]
+
+        return output
+
+print(Solution.longestCommonPrefix(["cir", "car"]))
