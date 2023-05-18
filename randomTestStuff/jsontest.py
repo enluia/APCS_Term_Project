cc1a89c5981c6f487300a3821d443033cc5bc03d
Json <jsty07@gmail.com>
2023-05-18 12:18:33 -0700

    test stuff to sync

--- randomTestStuff/jsontest.py
+++ randomTestStuff/jsontest.py
@@ -1,15 +1,7 @@
 import pandas as pd
 
-# read the data from the CSV file into a dataframe
-df = pd.read_csv('studentData.csv')
+# Read the CSV file
+df = pd.read_csv('studentData.csv', sep=',+', engine='python')
 
-# create a new column for IDs
-df['ID'] = df['Course'].str.startswith('ID').cumsum()  # assign ID based on "ID" rows
-
-# group the dataframe by ID
-grouped = df.groupby('ID')
-
-# create a dictionary of ID:data pairs
-data_dict = {}
-for name, group in grouped:
-    data_dict[group['ID'].iloc[0]] = group.drop('ID', axis=1).values.tolist()
+# Print the resulting DataFrame
+print(df.loc[5])
