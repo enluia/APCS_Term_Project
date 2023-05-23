112da27b166d99ec817ebcf349c65e50319861ac
WilliamK7827 <WilliamK7827@sd61learn.ca>
2023-05-23 13:06:28 -0700

    print courses

--- actualProjectStuff/SystemManager.py
+++ actualProjectStuff/SystemManager.py
@@ -8,16 +8,26 @@ import csv
 
 class SystemManager:
 
+    # students
     Parser.parse_raw_csv("Data for Project/Cleaned Student Requests.csv")
     parsed_data = Parser.read_parsed_csv("Data for Project/_parsedStudentData.csv")
 
     students = []
 
-    # Accessing the parsed data
     for set_id, course_ids in parsed_data.items():
         students.append(Student(set_id, course_ids))
 
     for i in students:
         print(i)
 
+    # courses
     CourseParser.parse_raw_csv("Data for Project/Course Information.csv")
+    parsed_course_data = CourseParser.read_parsed_csv("Data for Project/_parsedCourseData.csv")
+
+    courses = []
+
+    for name, base_terms, max_enroll, priority, sections in parsed_course_data.items():
+        courses.append(Course(name, base_terms, max_enroll, priority, sections))
+
+    for i in courses:
+        print(i)
