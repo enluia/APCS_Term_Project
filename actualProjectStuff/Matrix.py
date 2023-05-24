bec7af0a575a136f8f1f6f8f5dc8c7fa1cf20de0
WilliamK7827 <74074978+WilliamK7827@users.noreply.github.com>
2023-05-24 00:46:58 -0700

    Update Matrix.py

--- actualProjectStuff/Matrix.py
+++ actualProjectStuff/Matrix.py
@@ -1,7 +1,20 @@
 class Matrix:
 
-    matrix = []
+    def start(students, blocks, courses):
 
-    def start(students, courses):
-        print('yes')
+        # Define the matrix variable as a nested dictionary
+        matrix = {}
 
+        # Initialize the nested dictionaries for each key
+        for s_key in students:
+            matrix[s_key] = {}
+            for b in blocks:
+                matrix[s_key][b] = {}
+                for c_key in courses:
+                    matrix[s_key][b][c_key] = 0
+
+        # Loop through the matrix and print the values
+        for s_key in students:
+            for b in blocks:
+                for c_key in courses:
+                    print(s_key, b, c_key, matrix[s_key][b][c_key])
