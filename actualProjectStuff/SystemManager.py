6b1b9e383651a5a51339462ce3f6ee14ee756dbc
WilliamK7827 <74074978+WilliamK7827@users.noreply.github.com>
2023-05-24 00:47:55 -0700

    blocks
    blocks

--- actualProjectStuff/SystemManager.py
+++ actualProjectStuff/SystemManager.py
@@ -21,6 +21,9 @@ class SystemManager:
     for i in students:
         print(i)
 
+    # blocks
+    blocks = ['1A', '1B', '1C', '1D', '1E', '1F', '1G', '1H', '1I', '1J', '2A', '2B', '2C', '2D', '2E', '2F', '2G', '2H', '2I', '2J']
+
     # courses
     CourseParser.parse_raw_csv("Data for Project/Course Information.csv")
     parsed_course_data = CourseParser.read_parsed_csv("Data for Project/_parsedCourseData.csv")
