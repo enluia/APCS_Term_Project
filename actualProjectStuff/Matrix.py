0fc6d043cab87c473620506371146098dcdd5ed0
WilliamK7827 <WilliamK7827@sd61learn.ca>
2023-05-26 12:43:03 -0700

    yes

--- actualProjectStuff/Matrix.py
+++ actualProjectStuff/Matrix.py
@@ -43,28 +43,12 @@ class Matrix:
                         b_key += 1
 
 
-        # # Assign value of 1 for students chosen courses
-        # for s_key in students:
-        #     b_key = 0
-        #     ec_key = 8
-
-        #     # For every course a student has requested
-        #     for c_key in students[s_key]:
-
-        #         if (c_key in outside_timetable):
-        #             matrix[s_key][blocks[ec_key]][c_key] = 1
-        #             ec_key += 1
-        #         else:
-        #             matrix[s_key][blocks[b_key]][c_key] = 1
-        #             b_key += 1
-
-            
-
-
-
         # Loop through the matrix and print the values
         for s_key in students:
             for b in blocks:
                 for c_key in courses:
                     if matrix[s_key][b][c_key] == 1:
                         print(s_key, b, c_key, matrix[s_key][b][c_key])
+
+    def measure():
+        print()
