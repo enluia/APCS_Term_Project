7ab206a422f7cc6c2824505ff42ecc5697048b32
WilliamK7827 <wtk2.co@gmail.com>
2023-05-26 01:16:27 -0700

    to finish later

--- actualProjectStuff/ParserConditions.py
+++ actualProjectStuff/ParserConditions.py
@@ -19,3 +19,21 @@ class ParserConditions:
                 data.append(current_set)
 
         return data
+
+    def parse_blocking_csv(file_path):
+        data = []
+        current_set = None
+
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                
+                if row[1] == "" or row[1].startswith("Rule"):
+                    continue
+
+                current_set = {row[2].split()[1]: row[2].split(' before ')[1].split(', ')}
+
+                data.append(current_set)
+
+        return data
